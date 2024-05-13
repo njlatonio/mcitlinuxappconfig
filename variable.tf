90740cca53c9e10c8a51819ab57e7cc1767ad53e
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-13 18:22:00 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,38 +10,3 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
-
-variable "name_1"{
-  type=string
-  default="firstlinuxapp"
-}
-
-
-variable "name_2"{
-  type=string
-  default="secondlinuxapp"
-}
-variable "name_3"{
-  type=string
-  default="thirdlinuxapp"
-}
-
-/*
-#variable "os_type"{
-#  type=string
-#  default=Linux
-#}
-
-variable "sku_name_1"{
-#  type=string
-  default="P1v2"
-}
-variable "sku_name_2"{
-#  type=string
-  default="EP1"
-}
-variable "sku_name_3"{
-#  type=string
-  default="WS1"
-}
-*/
