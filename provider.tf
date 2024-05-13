bd6784bedaa450350d526e6c44d0888965f40f31
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-13 18:22:18 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -18,8 +18,3 @@ provider "azurerm"{
   tenant_id=var.tenant_id
 }
 
-
-resource "azurerm_resource_group" "azureresourcegroup_linuxapp" {
-  name     = "MCIT_lab_session_linuxapp"
-  location = "Canada Central"
-}
