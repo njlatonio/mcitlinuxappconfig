ee766c692faa42efbe44eba836114cd5086d11a6
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-13 17:52:57 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,28 +10,4 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
-variable "prefix"{
- type=string
- default="nichelle"
-}
-variable "account_tier"{
-  type=string
-  default="Standard"
-}
-variable "access_tier"{
-  type=string
-  default="Cool"
-}
-variable "account_replication_type"{
-  type=string
-  default="GRS"
-}
-variable "cross_tenant_replication_enabled"{
- type=bool
- default=false
-}
 
-variable "environment"{
- type=string
- default="staging"
-}
