337480f17194f1acd77799154f600d1e13743cca
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-13 20:21:59 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,13 @@
 output "azure_linux_app"{
   value = local.linux_app_list
 }
+
+output "linux_app_names"{
+  value = [for app in local.linux_app_list:app.name]
+}
+output "linux_app_os"{
+  value = [for app in local.linux_app_list:app.os]
+}
+output "linux_app_sku"{
+  value = [for app in local.linux_app_list:app.sku]
+}
